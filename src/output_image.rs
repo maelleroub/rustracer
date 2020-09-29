84bb55d62ec09ee3aee663b292cbc2c524faa7a5
Maelle Roubeau <maelle.roubeau@epita.fr>
2020-09-29 23:44:49 +0200

    print_image: cleaner printing, auxiliary functions for writing to file

--- src/output_image.rs
+++ src/output_image.rs
@@ -10,21 +10,29 @@ pub struct Image {
     pub pixels: Vec<Pixel>,
 }
 
+fn write_line_to_file(file: &fs::File, line: String) {
+    write_to_file(file, line + "\n");
+}
+
+fn write_to_file(mut file: &fs::File, line: String) {
+    file.write_all((line).as_bytes()).expect("Could not write to file");
+}
+
 pub fn print_image(image: &Image, path: &path::Path) {
-    let mut file = fs::File::create(&path).expect("Could not create output file");
-    file.write_all("P3\n".as_bytes()).expect("Could not write to file");
-    file.write_all((format!("{} {}\n", image.width, image.height)).as_bytes()).expect("Could not write to file");
-    file.write_all("255\n".as_bytes()).expect("Could not write to file");
+    let file = fs::File::create(&path).expect("Could not create output file");
+    write_line_to_file(&file, "P3".to_string());
+    write_line_to_file(&file, format!("{} {}", image.width, image.height));
+    write_line_to_file(&file, "255".to_string());
 
     for i in 0..image.height {
         for j in 0..image.width {
             let p = &image.pixels[i * image.width + j];
-            file.write_all((format!("{} {} {}", p.0, p.1, p.2)).as_bytes()).expect("Could not write to file");
+            write_to_file(&file, format!("{} {} {}", p.0, p.1, p.2).to_string());
             let mut c = ' ';
             if j == (image.width - 1) {
                 c = '\n';
             }
-            file.write_all(c.to_string().as_bytes()).expect("Could not write to file");
+            write_to_file(&file, c.to_string());
         }
     }
 }
