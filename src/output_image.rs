8e65de505507117832b1db9c356d13f972c0c676
Maelle Roubeau <maelle.roubeau@epita.fr>
2020-09-29 23:57:49 +0200

    image: print is now a method of Image

--- src/output_image.rs
+++ src/output_image.rs
@@ -10,29 +10,31 @@ pub struct Image {
     pub pixels: Vec<Pixel>,
 }
 
+impl Image {
+    pub fn print(&self, path: &path::Path) {
+        let file = fs::File::create(&path).expect("Could not create output file");
+        write_line_to_file(&file, "P3".to_string());
+        write_line_to_file(&file, format!("{} {}", self.width, self.height));
+        write_line_to_file(&file, "255".to_string());
+
+        for i in 0..self.height {
+            for j in 0..self.width {
+                let p = &self.pixels[i * self.width + j];
+                write_to_file(&file, format!("{} {} {}", p.0, p.1, p.2).to_string());
+                let mut c = ' ';
+                if j == (self.width - 1) {
+                    c = '\n';
+                }
+                write_to_file(&file, c.to_string());
+            }
+        }
+    }
+}
+
 fn write_line_to_file(file: &fs::File, line: String) {
     write_to_file(file, line + "\n");
 }
 
 fn write_to_file(mut file: &fs::File, line: String) {
     file.write_all((line).as_bytes()).expect("Could not write to file");
-}
-
-pub fn print_image(image: &Image, path: &path::Path) {
-    let file = fs::File::create(&path).expect("Could not create output file");
-    write_line_to_file(&file, "P3".to_string());
-    write_line_to_file(&file, format!("{} {}", image.width, image.height));
-    write_line_to_file(&file, "255".to_string());
-
-    for i in 0..image.height {
-        for j in 0..image.width {
-            let p = &image.pixels[i * image.width + j];
-            write_to_file(&file, format!("{} {} {}", p.0, p.1, p.2).to_string());
-            let mut c = ' ';
-            if j == (image.width - 1) {
-                c = '\n';
-            }
-            write_to_file(&file, c.to_string());
-        }
-    }
 }
