853975f87820b2e43968af3fc3b582d1be0ec982
Maelle Roubeau <maelle.roubeau@epita.fr>
2020-10-02 00:57:36 +0200

    ray: hit_sphere

--- src/ray.rs
+++ src/ray.rs
@@ -10,8 +10,19 @@ impl Ray {
         self.origin + (self.direction * t)
     }
     pub fn color(&self) -> Vec3 {
+        if self.hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5) {
+            return Vec3(1.0, 0.0, 0.0);
+        }
         let unit_direction: Vec3 = self.direction.normalize();
         let t: f64 = 0.5 * (unit_direction.1 + 1.0);
         Vec3(1.0, 1.0, 1.0) * (1.0 - t) + (Vec3(0.5, 0.7, 1.0) * t)
+    }
+    pub fn hit_sphere(&self, center: Vec3, radius: f64) -> bool {
+        let oc = self.origin - center;
+        let a = Vec3::dot(self.direction, self.direction);
+        let b = Vec3::dot(oc, self.direction) * 2.0;
+        let c = Vec3::dot(oc, oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
     }
 }
