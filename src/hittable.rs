474fe82713325b5f4b4fb0e6e051c0c7c91ee6ae
Maelle Roubeau <maelle.roubeau@epita.fr>
2020-10-02 16:20:44 +0200

    hittable: added front_face to HitRecord, and function to set it (6.4 Listing 18)

--- src/hittable.rs
+++ src/hittable.rs
@@ -4,7 +4,19 @@ use super::vec3::Vec3;
 pub struct HitRecord {
     pub p: Vec3,
     pub normal: Vec3,
-    pub t: f64
+    pub t: f64,
+    pub front_face: bool
+}
+
+impl HitRecord {
+    #[inline]
+    pub fn set_face_normal(&mut self, r: Ray, outward_normal: Vec3) {
+        self.front_face = Vec3::dot(r.direction, outward_normal) < 0.0;
+        self.normal = match self.front_face {
+            true => outward_normal,
+            false => (-1.0) * outward_normal
+        };
+    }
 }
 
 pub trait Hittable {
